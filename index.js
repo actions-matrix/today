aca90b2c2e75edbd65249d9d68d891f67be175a1
Socheat Sok <socheatsok78@gmail.com>
2023-07-15 02:03:28 +0700

    Install @actions/core

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const process = require('process');
+const core = require('@actions/core');
 
-const set_output = (name, value) => {
-    fs.appendFileSync(process.env.GITHUB_OUTPUT, `${name}=${value}}`);
-    console.log("Set output: " + name + "=" + value);
+async function run() {
+    const today = new Date()
+
+    const output = {
+        date: `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`,
+        year: today.getFullYear(),
+        month: today.getMonth() + 1,
+        day: today.getDate(),
+    }
+
+    core.setOutput('date', `${output.date}`)
+    core.setOutput('year', output.year)
+    core.setOutput('month', output.month)
+    core.setOutput('day', output.day)
 }
 
-const today = new Date()
-
-const output = {
-    date: `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`,
-    year: today.getFullYear(),
-    month: today.getMonth() + 1,
-    day: today.getDate(),
-}
-
-set_output('date', `${output.date}`)
-set_output('year', output.year)
-set_output('month', output.month)
-set_output('day', output.day)
+run()
